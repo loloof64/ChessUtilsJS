299cb06213554c2b95a7d42885f09e74cfb42176
Laurent Bernabe <laurent.bernabe@gmail.com>
2018-06-24 01:03:27 +0200

    Simple reorganisation.

--- test/Pawn.js
+++ test/Pawn.js
@@ -1,16 +1,16 @@
 import * as assert from 'assert';
 import { Pawn } from '../src/core/pieces';
 
-describe('A chess piece', function() {
+describe('A pawn', function() {
   describe('#toFen()', function() {
     let testedPiece;
 
-    it('should return "P" if is a white pawn', function() {
+    it('should return "P" if is white', function() {
       testedPiece = new Pawn(false);
       assert.equal(testedPiece.toFen(), 'P');
     });
 
-    it('should return "p" if is a black pawn', function() {
+    it('should return "p" if is black', function() {
         testedPiece = new Pawn(true);
         assert.equal(testedPiece.toFen(), 'p');
       });
